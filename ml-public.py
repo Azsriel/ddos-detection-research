7371f18b56154996a5afdb0168bdef21fcd0ea57
aszriel <sfurqaanl@gmail.com>
2025-07-27 18:33:52 +0530

    ml-public is main file

--- ml-public.py
+++ ml-public.py
@@ -12,7 +12,7 @@ import joblib
 df = pd.read_parquet("CIC-DDoS2019-combined.parquet", engine="pyarrow")
 df = df.dropna()
 
-'''
+
 # Turn the Labels into Binary - Either Benign or DDoS
 # Normalize label case
 df["Label"] = df["Label"].str.strip().str.lower()
@@ -24,15 +24,15 @@ df["BinaryLabel"] = df["Label"].apply(lambda x: "Benign" if "benign" in x else "
 X = df.drop(columns=["Label", "BinaryLabel"])
 y = df["BinaryLabel"]
 
-'''
 
+'''
 # If not converting to Binary then do this otherwise comment out
 X = df.drop(columns=["Label"])
 y = df["Label"]
-
+'''
 
 # Add unsupervised features
-iso = IsolationForest(contamination=0.1)
+iso = IsolationForest(contamination=0.25)
 df["anomaly_score"] = iso.fit_predict(X)
 
 kmeans = KMeans(n_clusters=2)
@@ -42,7 +42,8 @@ df["cluster_id"] = kmeans.fit_predict(X)
 X_aug = pd.concat([X, df[["anomaly_score", "cluster_id"]]], axis=1)
 
 # Train/Test Split
-X_train, X_test, y_train, y_test = train_test_split(X_aug, y, test_size=0.3, random_state=42)
+# Change to X instead of X_aug inside the function to get values for only C4.5
+X_train, X_test, y_train, y_test = train_test_split(X_aug, y, test_size=0.99, random_state=42)
 
 # Train C4.5-like model
 clf = DecisionTreeClassifier(criterion="entropy")
@@ -54,4 +55,4 @@ joblib.dump(clf, "trained_model.joblib")
 # Offline evaluation
 y_pred = clf.predict(X_test)
 print("\n=== Public Dataset Evaluation ===")
-print(classification_report(y_test, y_pred))
+print(classification_report(y_test, y_pred, digits=4))
